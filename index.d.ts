5468a1fa8aa9429e5d81f5c8dde41d9dbb2e34c0
John <webdeveloper@johnpuaoi.tech>
2021-10-05 14:27:16 -1000

    new vers 1.0.2, added copyTextToClipboard

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,4 @@
 /** This function is for use with firebase timestamps, pass in the timestamp and then if time is desired pass the truthy returnTime parameter. */
 export declare const formatDate: (timestamp: number, returnTime?: boolean | undefined) => string;
 export declare const isEmptyObject: (obj: object) => Boolean;
+export declare const copyTextToClipboard: (textToCopy: string) => Promise<void>;
